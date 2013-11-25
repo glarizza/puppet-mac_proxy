e4ea27285bb58df25902e0936b5b0cffbaff7ae7
Gary Larizza <gary@puppetlabs.com>
2013-11-24 20:18:26 -0800

    Document Puppet::Provider::MacProxy
    Previously, my documentation was pretty poor for the
    Puppet::Provider::MacProxy method (which is used by all
    of the proxy types).  This commit adds documentation that
    includes some useful information about self.instances,
    self.prefetch, and flush.

--- lib/puppet/provider/mac_proxy.rb
+++ lib/puppet/provider/mac_proxy.rb
@@ -1,4 +1,10 @@
 class Puppet::Provider::MacProxy < Puppet::Provider
+  # This is a helper library class that's shared by all the Mac proxy types.
+  # Inside this class are all the methods necessary to 'do the work' for the
+  # types - all the providers for those types need to do are inherit from this
+  # base class and define a method called 'self.get_proxy_type' that returns
+  # the symbolized value for the type of proxy that's being declared.
+
   def self.proxy_mapping
   # This method returns a hash mapping the proxy_type with the arguments
   # necessary for the `networksetup` binary. The 'auto' proxy type is different
@@ -50,17 +56,46 @@ class Puppet::Provider::MacProxy < Puppet::Provider
   end
 
   def initialize(value={})
+  # The @property_flush variable is necessary for the provider to destroy, or
+  # ensure absent, the instance. All other initialization can be left to the
+  # parent classes.
     super(value)
     @property_flush = {}
   end
 
   def self.get_list_of_interfaces
+  # This method returns an array of all 'Friendly' interface names (a la
+  # 'Ethernet' or 'FireWire').
     interfaces = networksetup('-listallnetworkservices').split("\n")
     interfaces.shift
     interfaces.sort
   end
 
+  def self.instances
+  # self.instances returns all instances of the resource type that are
+  # discovered on the system.  The self.instances method is used by `puppet
+  # resource`, and MUST be implemented for `puppet resource` to work. The
+  # self.instances method is also frequently used by self.prefetch (which is
+  # also the case for this provider class).
+    interfaces = get_list_of_interfaces
+    array_of_interfaces = interfaces.collect do |int|
+      proxy_properties = get_proxy_properties(int, get_proxy_type)
+      new(proxy_properties)
+    end
+    array_of_interfaces
+  end
+
   def self.prefetch(resources)
+  # Prefetching is invoked when managing a resource with `puppet agent`,
+  # `puppet apply`, or using `puppet resource` to change a resource from the
+  # command line. The self.prefetch method accepts a hash of all managed
+  # resources for the mac proxy types (i.e. all resources of this type that
+  # are in the catalog). The method also populates the @property_hash instance
+  # variable with property values for each managed resource, and
+  # @property_hash can be used by all provider methods (i.e. all methods that
+  # DON'T begin with 'self.'). In this case (as is the case with most
+  # prefetching methods), we're using self.instances to discover all property
+  # values.
     instances.each do |prov|
       if resource = resources[prov.name]
         resource.provider = prov
@@ -77,8 +112,8 @@ class Puppet::Provider::MacProxy < Puppet::Provider
   end
 
   def self.get_proxy_properties(int, proxy_type)
-    # This method will return a hash containing all the web proxy 'IS' value
-    # properties for a provided interface
+  # This method will return a hash containing all the web proxy 'IS' value
+  # properties for a provided interface
     interface_properties = {}
 
     begin
@@ -122,11 +157,13 @@ class Puppet::Provider::MacProxy < Puppet::Provider
 
     # All proxy types share these two properties
     interface_properties[:provider]             = :ruby
-    interface_properties[:name]                 = int
+    interface_properties[:name]                 = int.downcase
     interface_properties
   end
 
   def set_proxy(proxy_type)
+  # The set_proxy method wraps the process of setting proxy values for
+  # an interface.
     if @property_flush[:ensure] == :absent
         networksetup([self.class.proxy_mapping[proxy_type]['enable_argument'], resource[:name], 'off'])
         return
@@ -176,26 +213,25 @@ class Puppet::Provider::MacProxy < Puppet::Provider
   end
 
   def flush
+  # The flush provider method is invoked ONCE per resource whenever any of
+  # the resource's properties need to be synchronized/changed. It allows for
+  # setting all properties of a resource at once, so gives you a bit of
+  # a performance boost (instead of invoking individual setter methods for
+  # every property).  Flush allows you to make a single system call that will
+  # set multiple property values - but in this case, because networksetup
+  # requires multiple arguments in order to set the multiple properties for
+  # a proxy, we are only going to make a single call for each resource that
+  # needs to be changed.
     if @property_flush
       set_proxy(get_proxy_type)
     end
-    # Collect the resources again once they've been changed
+    # Collect the resources again once they've been changed (that way `puppet
+    # resource` will show the correct values after changes have been made).
     @property_hash = self.class.get_proxy_properties(resource[:name], get_proxy_type)
   end
 
   def create
     set_proxy(get_proxy_type)
   end
-
-  def self.instances
-  # Necessary for `puppet resource` and ultimately prefetching
-  # NOTE: Prefetching is performed inside Puppet::Provider::MacNetwork,
-  #       we're inheriting that behavior inside this class.
-    interfaces = get_list_of_interfaces
-    array_of_interfaces = interfaces.collect do |int|
-      proxy_properties = get_proxy_properties(int, get_proxy_type)
-      new(proxy_properties)
-    end
-    array_of_interfaces
-  end
 end
+
